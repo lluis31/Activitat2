000c969fedbe4a31e226a8be3d595365845aadfd
Lluis planas <gam_lluis@hotmail.com>
2020-12-22 17:47:38 +0100

    Agregar archivos de proyecto.

---
+++ activitat2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace activitat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Writeline("HOLA MUNDO!!!");
+            int num1 = 5;
+            int num2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es:" + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+                }
+    }
+}
